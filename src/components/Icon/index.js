d205d5de9e633555d8b19f72fd14a2c4b7f38e33
Bruno <brunodasilvalenga@gmail.com>
2019-07-03 17:10:41 +1000

    add icon ; refactoring component

--- src/components/Icon/index.js
+++ src/components/Icon/index.js
@@ -1,16 +1,18 @@
-import React from 'react'
-
+import React, { cloneElement } from 'react'
 import { ReactComponent as SingleSpace } from './svg/single-space.svg'
 import { ReactComponent as MultipleSpace } from './svg/multiple-space.svg'
+import { ReactComponent as Business } from './svg/business.svg'
+
+const Icons = {
+  'single-space': <SingleSpace />,
+  'multiple-space': <MultipleSpace />,
+  business: <Business />
+}
 
 const Icon = props => {
-  switch (props.name) {
-    case 'single-space':
-      return <SingleSpace {...props} />
-    case 'multiple-space':
-      return <MultipleSpace {...props} />
-    default:
-      return null
+  if (!props.name) {
+    return null
   }
+  return cloneElement(Icons[props.name], props)
 }
 export default Icon
