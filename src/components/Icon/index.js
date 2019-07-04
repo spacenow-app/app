b48ed5dde459ffe3dcecad870356c9223cbcb258
Bruno <brunodasilvalenga@gmail.com>
2019-07-04 15:35:37 +1000

    create new icons

--- src/components/Icon/index.js
+++ src/components/Icon/index.js
@@ -1,16 +1,92 @@
 import React, { cloneElement } from 'react'
 import { ReactComponent as SingleSpace } from './svg/single-space.svg'
 import { ReactComponent as MultipleSpace } from './svg/multiple-space.svg'
-import { ReactComponent as Business } from './svg/business.svg'
+//Category
+import { ReactComponent as CategoryCoworking } from './svg/category/category_coworking.svg'
+import { ReactComponent as CategoryDesk } from './svg/category/category_desk.svg'
+import { ReactComponent as CategoryEvents } from './svg/category/category_events.svg'
+import { ReactComponent as CategoryHospitality } from './svg/category/category_hospitality.svg'
+import { ReactComponent as CategoryMeetingRoom } from './svg/category/category_meeting_room.svg'
+import { ReactComponent as CategoryOffice } from './svg/category/category_office.svg'
+import { ReactComponent as CategoryParking } from './svg/category/category_parking.svg'
+import { ReactComponent as CategoryStorage } from './svg/category/category_storage.svg'
+//Sub-Category
+import { ReactComponent as SubCategoryBoardRoom } from './svg/sub-category/sub_category_board_room.svg'
+import { ReactComponent as SubCategoryBusiness } from './svg/sub-category/sub_category_business.svg'
+import { ReactComponent as SubCategoryCafe } from './svg/sub-category/sub_category_cafe.svg'
+import { ReactComponent as SubCategoryCarPark } from './svg/sub-category/sub_category_car_park.svg'
+import { ReactComponent as SubCategoryCarRvBoat } from './svg/sub-category/sub_category_car_rv_boat.svg'
+import { ReactComponent as SubCategoryConferenceRoom } from './svg/sub-category/sub_category_conference_room.svg'
+import { ReactComponent as SubCategoryConference } from './svg/sub-category/sub_category_conference.svg'
+import { ReactComponent as SubCategoryCorporate } from './svg/sub-category/sub_category_corporate.svg'
+import { ReactComponent as SubCategoryCreative } from './svg/sub-category/sub_category_creative.svg'
+import { ReactComponent as SubCategoryDesk } from './svg/sub-category/sub_category_desk.svg'
+import { ReactComponent as SubCategoryDriveway } from './svg/sub-category/sub_category_driveway.svg'
+import { ReactComponent as SubCategoryEntireOffice } from './svg/sub-category/sub_category_entire_office.svg'
+import { ReactComponent as SubCategoryFoodtruck } from './svg/sub-category/sub_category_foodtruck.svg'
+import { ReactComponent as SubCategoryFunctionCentre } from './svg/sub-category/sub_category_function_centre.svg'
+import { ReactComponent as SubCategoryGarage } from './svg/sub-category/sub_category_garage.svg'
+import { ReactComponent as SubCategoryHealthFitness } from './svg/sub-category/sub_category_health_fitness.svg'
+import { ReactComponent as SubCategoryKitchen } from './svg/sub-category/sub_category_kitchen.svg'
+import { ReactComponent as SubCategoryMarketStalls } from './svg/sub-category/sub_category_market_stalls.svg'
+import { ReactComponent as SubCategoryMeetingRoom } from './svg/sub-category/sub_category_meeting_room.svg'
+import { ReactComponent as SubCategoryPersonal } from './svg/sub-category/sub_category_personal.svg'
+import { ReactComponent as SubCategoryPopupRetail } from './svg/sub-category/sub_category_popup_retail.svg'
+import { ReactComponent as SubCategoryPrivateOffice } from './svg/sub-category/sub_category_private_office.svg'
+import { ReactComponent as SubCategoryPrivateRoom } from './svg/sub-category/sub_category_private_room.svg'
+import { ReactComponent as SubCategoryRestaurant } from './svg/sub-category/sub_category_restaurant.svg'
+import { ReactComponent as SubCategoryRetail } from './svg/sub-category/sub_category_retail.svg'
+import { ReactComponent as SubCategorySports } from './svg/sub-category/sub_category_sports.svg'
+import { ReactComponent as SubCategoryTrainingRoom } from './svg/sub-category/sub_category_training_room.svg'
+import { ReactComponent as SubCategoryVenue } from './svg/sub-category/sub_category_venue.svg'
+import { ReactComponent as SubCategoryWeddingVenue } from './svg/sub-category/sub_category_wedding_venue.svg'
+import { ReactComponent as SubCategoryWineStorage } from './svg/sub-category/sub_category_wine_storage.svg'
 
 const Icons = {
   'single-space': <SingleSpace />,
   'multiple-space': <MultipleSpace />,
-  business: <Business />
+  'category-coworking': <CategoryCoworking />,
+  'category-desk': <CategoryDesk />,
+  'category-events': <CategoryEvents />,
+  'category-hospitality': <CategoryHospitality />,
+  'category-meeting-room': <CategoryMeetingRoom />,
+  'category-office': <CategoryOffice />,
+  'category-parking': <CategoryParking />,
+  'category-storage': <CategoryStorage />,
+  'sub-category-board-room': <SubCategoryBoardRoom />,
+  'sub-category-business': <SubCategoryBusiness />,
+  'sub-category-cafe': <SubCategoryCafe />,
+  'sub-category-car-park': <SubCategoryCarPark />,
+  'sub-category-car-rv-boat': <SubCategoryCarRvBoat />,
+  'sub-category-conference-room': <SubCategoryConferenceRoom />,
+  'sub-category-conference': <SubCategoryConference />,
+  'sub-category-corporate': <SubCategoryCorporate />,
+  'sub-category-creative': <SubCategoryCreative />,
+  'sub-category-desk': <SubCategoryDesk />,
+  'sub-category-driveway': <SubCategoryDriveway />,
+  'sub-category-entire-office': <SubCategoryEntireOffice />,
+  'sub-category-foodtruck': <SubCategoryFoodtruck />,
+  'sub-category-function-centre': <SubCategoryFunctionCentre />,
+  'sub-category-garage': <SubCategoryGarage />,
+  'sub-category-health-fitness': <SubCategoryHealthFitness />,
+  'sub-category-kitchen': <SubCategoryKitchen />,
+  'sub-category-market-stalls': <SubCategoryMarketStalls />,
+  'sub-category-meeting-room': <SubCategoryMeetingRoom />,
+  'sub-category-personal': <SubCategoryPersonal />,
+  'sub-category-popup-retail': <SubCategoryPopupRetail />,
+  'sub-category-private-office': <SubCategoryPrivateOffice />,
+  'sub-category-private-room': <SubCategoryPrivateRoom />,
+  'sub-category-restaurant': <SubCategoryRestaurant />,
+  'sub-category-retail': <SubCategoryRetail />,
+  'sub-category-sports': <SubCategorySports />,
+  'sub-category-training-room': <SubCategoryTrainingRoom />,
+  'sub-category-venue': <SubCategoryVenue />,
+  'sub-category-wedding-venue': <SubCategoryWeddingVenue />,
+  'sub-category-wine-storage': <SubCategoryWineStorage />
 }
 
 const Icon = props => {
-  if (!props.name) {
+  if (!Icons[props.name]) {
     return null
   }
   return cloneElement(Icons[props.name], props)
