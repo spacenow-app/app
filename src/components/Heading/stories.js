7219b2215f62f0589256463414830bbef548c2d5
Bruno <brunodasilvalenga@gmail.com>
2019-07-08 16:35:55 +1000

    remove component duplicate -> Title

--- src/components/Heading/stories.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-import { action } from '@storybook/addon-actions';
-import Heading from './Heading';
-
-storiesOf('Heading', module).add('Default', () => (
-  <Heading onClick={action('clicked')}>Hello Heading</Heading>
-));
