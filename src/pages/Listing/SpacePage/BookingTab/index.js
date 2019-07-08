4d7c22de214c5b1f1248fd4d51b5cdaa1f8a8461
Bruno <brunodasilvalenga@gmail.com>
2019-07-08 16:36:31 +1000

    create new tabs

---
+++ src/pages/Listing/SpacePage/BookingTab/index.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+import { Wrapper } from 'components'
+
+class BookingTab extends Component {
+  render() {
+    return <Wrapper>BookingTab</Wrapper>
+  }
+}
+
+export default BookingTab
--- src/pages/Listing/SpacePage/SpecificationTab/index.js
+++ src/pages/Listing/SpacePage/SpecificationTab/index.js
@@ -1,9 +1,114 @@
 import React, { Component } from 'react'
-import { Wrapper } from 'components'
+import styled from 'styled-components'
+import { Title, Input, Checkbox, Select, TextArea } from 'components'
 
-export class SpecificationTab extends Component {
+const WrapperStyled = styled.div`
+  display: grid;
+  grid-row-gap: 80px;
+`
+
+const SectionStyled = styled.div``
+
+const InputGroup = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
+  grid-column-gap: 30px;
+`
+
+const CheckboxGroup = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  grid-row-gap: 40px;
+`
+
+class SpecificationTab extends Component {
   render() {
-    return <Wrapper>SpecificationTab</Wrapper>
+    return (
+      <WrapperStyled>
+        <SectionStyled>
+          <Title
+            type="h3"
+            title="Title*"
+            subtitle="Your title sets the scene. Make it short, powerful and identify someone’s need for it."
+          />
+          <Input label="Description" placeholder="e.g. Car park 100m walk to Central Station" />
+        </SectionStyled>
+        <SectionStyled>
+          <Title
+            type="h3"
+            title="Specifications*"
+            subtitle="Give users the quick highlights of the space. These are also important search criteria for guests to find their perfect space."
+          />
+          <InputGroup>
+            <Input label="Capacity" placeholder="Specification" />
+            <Input label="Size" placeholder="Specification" />
+            <Input label="Car Space" placeholder="Specification" />
+            <Select label="Type" />
+          </InputGroup>
+        </SectionStyled>
+        <SectionStyled>
+          <Title
+            type="h3"
+            title="About"
+            subtitle="Sell ‘em the dream. Start broad, then get specific. Include features and details that make your space special. Don’t forget to mention the light, he ambience, the vibe plus any good transport options. Local food options or other local gems. Be honest, but be persuasive."
+          />
+          <TextArea placeholder="Describe your space" />
+        </SectionStyled>
+        <SectionStyled>
+          <Title type="h3" title="Amenities" subtitle="What features does your space offer guests?" />
+          <CheckboxGroup>
+            <Checkbox label="Amenity name" />
+            <Checkbox label="Amenity name" />
+            <Checkbox label="Amenity name" />
+            <Checkbox label="Amenity name" />
+            <Checkbox label="Amenity name" />
+            <Checkbox label="Amenity name" />
+            <Checkbox label="Amenity name" />
+            <Checkbox label="Amenity name" />
+            <Checkbox label="Amenity name" />
+            <Checkbox label="Amenity name" />
+          </CheckboxGroup>
+        </SectionStyled>
+        <SectionStyled>
+          <Title type="h3" title="Space Rules" subtitle="Let guests know about the rules of the space." />
+          <CheckboxGroup>
+            <Checkbox label="Keep noise/music within regulations" />
+            <Checkbox label="Amenity name" />
+            <Checkbox label="Amenity name" />
+            <Checkbox label="Amenity name" />
+            <Checkbox label="Amenity name" />
+            <Checkbox label="Vacate space within time allocated" />
+            <Checkbox label="Not suitable for children" />
+            <Checkbox label="Amenity name" />
+            <Checkbox label="Amenity name" />
+            <Checkbox label="Remove rubbish" />
+          </CheckboxGroup>
+        </SectionStyled>
+        <SectionStyled>
+          <Title type="h3" title="Access Information*" subtitle="Let your guests know how they’ll get in." />
+          <Select />
+        </SectionStyled>
+        <SectionStyled>
+          <Title
+            type="h3"
+            title="Photos*"
+            subtitle="Photos help guests imagine using your space. You can start with one and add more after you publish."
+          />
+          <div style={{ display: 'grid', gridTemplateColumns: 'auto auto auto auto auto auto' }}>
+            <div style={{ width: '150px', height: '150px', border: '1px solid black' }} />
+            <div style={{ width: '150px', height: '150px', border: '1px solid black' }} />
+            <div style={{ width: '150px', height: '150px', border: '1px solid black' }} />
+            <div style={{ width: '150px', height: '150px', border: '1px solid black' }} />
+            <div style={{ width: '150px', height: '150px', border: '1px solid black' }} />
+            <div style={{ width: '150px', height: '150px', border: '1px solid black' }} />
+          </div>
+          <p>
+            TIP: Take photos in landscape mode to capture as much of your space as possible. Shoot from corners to add
+            perspective. Spaces look best in natural light. Include all areas your guest can access.
+          </p>
+        </SectionStyled>
+      </WrapperStyled>
+    )
   }
 }
 
