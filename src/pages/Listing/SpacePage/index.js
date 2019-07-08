dfd3bf4a392007e3de3cd32c9e3bba3e23b78f71
Bruno <brunodasilvalenga@gmail.com>
2019-07-08 16:36:57 +1000

    include route and tabs

--- src/pages/Listing/SpacePage/index.js
+++ src/pages/Listing/SpacePage/index.js
@@ -1,36 +1,44 @@
 import React from 'react'
-import { Switch, Route, Link, Redirect } from 'react-router-dom'
+import { Switch, Route, Redirect } from 'react-router-dom'
 import CancellationTab from './CancellationTab'
-import PricingTab from './PricingTab'
+import BookingTab from './BookingTab'
 import SpecificationTab from './SpecificationTab'
-import TimeTab from './TimeTab'
+import AvailabilityTab from './AvailabilityTab'
+import { Wrapper, Tab, TabItem, StepButtons } from 'components'
 
-const SpacePage = ({ match, ...props }) => {
+const SpacePage = ({ match, location, ...props }) => {
   return (
-    <>
-      <div className="links">
-        <Link to={`specification`} className="link">
-          Spec
-        </Link>
-        <Link to={`pricing`} className="link">
-          Pric
-        </Link>
-        <Link to={`time`} className="link">
-          Time
-        </Link>
-        <Link to={`cancellation`} className="link">
-          Cancela
-        </Link>
-      </div>
+    <Wrapper>
+      <Tab>
+        <TabItem nav to={`${match.url}/specification`}>
+          Specification
+        </TabItem>
+        <TabItem nav to={`${match.url}/booking`}>
+          Booking
+        </TabItem>
+        <TabItem nav to={`${match.url}/availability`}>
+          Availability
+        </TabItem>
+        <TabItem nav to={`${match.url}/cancellation`}>
+          Cancelation Policy
+        </TabItem>
+      </Tab>
+
       <Switch>
         <Redirect exact from={match.path} to={`${match.path}/specification`} />
         <Route path={`${match.path}/specification`} component={SpecificationTab} />
-        <Route exact path={`${match.path}/pricing`} component={PricingTab} />
-        <Route path={`${match.path}/time`} component={TimeTab} />
+        <Route path={`${match.path}/booking`} component={BookingTab} />
+        <Route path={`${match.path}/availability`} component={AvailabilityTab} />
         <Route path={`${match.path}/cancellation`} component={CancellationTab} />
         <Route component={() => <h1>not found</h1>} />
       </Switch>
-    </>
+      <StepButtons
+        prev={{ disabled: false, onClick: () => props.history.goBack() }}
+        next={{
+          onClick: () => props.history.push('/listing/pricing')
+        }}
+      />
+    </Wrapper>
   )
 }
 
