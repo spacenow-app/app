848305e997a4c37cadd72f8d927d8c524c606c58
Bruno <brunodasilvalenga@gmail.com>
2019-07-11 09:20:46 +1000

    add new font

--- src/styles/global.js
+++ src/styles/global.js
@@ -3,6 +3,7 @@ import { createGlobalStyle } from 'styled-components'
 import MontserratRegular from './default-theme/lib/fonts/Montserrat/Montserrat-Regular.ttf'
 import MontserratMedium from './default-theme/lib/fonts/Montserrat/Montserrat-Medium.ttf'
 import MontserratBold from './default-theme/lib/fonts/Montserrat/Montserrat-Bold.ttf'
+import MontserratSemiBold from './default-theme/lib/fonts/Montserrat/Montserrat-SemiBold.ttf'
 import Boostrap from './default-theme/lib/bootstrap/css/bootstrap.min.css'
 
 export default createGlobalStyle`
@@ -21,6 +22,10 @@ export default createGlobalStyle`
     @font-face{
       font-family: 'Montserrat-Bold';
       src: url(${MontserratBold});
+    }
+    @font-face{
+      font-family: 'Montserrat-SemiBold';
+      src: url(${MontserratSemiBold});
     }
 
     @import url(${Boostrap});
