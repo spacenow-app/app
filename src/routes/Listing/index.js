3c16643cd0330dc34e094ffac2b7a9f18ea765f8
Bruno Valenga <brunodasilvalenga@Bruno-Valenga-iMac.local>
2019-07-12 11:44:21 +1000

    configure routes to use lazyloading

--- src/routes/Listing/index.js
+++ src/routes/Listing/index.js
@@ -1,10 +1,16 @@
-import React from 'react'
+import React, { Suspense, lazy } from 'react'
+import PropTypes from 'prop-types'
 import { Switch, Route, Redirect } from 'react-router-dom'
-import { NotFoundPage, IntroPage, LocationPage, CategoryPage, SpacePage } from 'pages'
+
+const IntroPage = lazy(() => import('pages/Listing/IntroPage'))
+const LocationPage = lazy(() => import('pages/Listing/LocationPage'))
+const CategoryPage = lazy(() => import('pages/Listing/CategoryPage'))
+const SpacePage = lazy(() => import('pages/Listing/SpacePage'))
+const NotFoundPage = lazy(() => import('pages/NotFoundPage'))
 
 const Listing = ({ match, ...props }) => {
   return (
-    <>
+    <Suspense fallback={<div>Loading...</div>}>
       <Switch>
         <Redirect exact from={match.path} to={`${match.path}/intro`} />
         <Route exact component={IntroPage} path={`${match.path}/intro`} />
@@ -14,8 +20,12 @@ const Listing = ({ match, ...props }) => {
         <Route exact component={() => <h1>Prview</h1>} path={`${match.path}/preview/:id`} />
         <Route component={NotFoundPage} />
       </Switch>
-    </>
+    </Suspense>
   )
+}
+
+Listing.propTypes = {
+  match: PropTypes.objectOf(Object)
 }
 
 export default Listing
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,13 @@
-import React from 'react'
+import React, { Suspense, lazy } from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import { useSelector } from 'react-redux'
+import { NavBar } from 'components'
 import PrivateRoute from './PrivateRoute'
 import PublicRoute from './PublicRoute'
-import Listing from './Listing'
-import { NavBar } from 'components'
-import { HomePage, NotFoundPage } from 'pages'
+
+const HomePage = lazy(() => import('pages/HomePage'))
+const NotFoundPage = lazy(() => import('pages/NotFoundPage'))
+const Listing = lazy(() => import('routes/Listing'))
 
 const Routes = props => {
   const isAuthenticated = useSelector(state => state.auth.isAuthenticated)
@@ -17,39 +19,40 @@ const Routes = props => {
 
   return (
     <BrowserRouter>
-      <Switch>
-        <PublicRoute exact path="/auth" isAuthenticated={isAuthenticated} component={() => <h1>Login Page</h1>} />
-        <PublicRoute  path="/test" component={() => <h1>Test Page</h1>} />
-        <PrivateRoute
-          {...props}
-          path="/"
-          isAuthenticated={isAuthenticated}
-          component={props => {
-            return (
-              <>
-                <NavBar />
-                <Switch>
-                  <Route
-                    {...props}
-                    exact
-                    path={props.match.path}
-                    isAuthenticated={isAuthenticated}
-                    component={HomePage}
-                  />
-                  <Route
-                    {...props}
-                    path={`${props.match.path}listing`}
-                    isAuthenticated={isAuthenticated}
-                    component={Listing}
-                  />
-                  <Route component={NotFoundPage} />
-                </Switch>
-              </>
-            )
-          }}
-        />
-        <Route component={NotFoundPage} />
-      </Switch>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Switch>
+          <PublicRoute exact path="/auth" isAuthenticated={isAuthenticated} component={() => <h1>Login Page</h1>} />
+          <PrivateRoute
+            {...props}
+            path="/"
+            isAuthenticated={isAuthenticated}
+            component={otherProps => {
+              return (
+                <>
+                  <NavBar />
+                  <Switch>
+                    <Route
+                      {...otherProps}
+                      exact
+                      path={otherProps.match.path}
+                      isAuthenticated={isAuthenticated}
+                      component={HomePage}
+                    />
+                    <Route
+                      {...otherProps}
+                      path={`${otherProps.match.path}listing`}
+                      isAuthenticated={isAuthenticated}
+                      component={Listing}
+                    />
+                    <Route component={NotFoundPage} />
+                  </Switch>
+                </>
+              )
+            }}
+          />
+          <Route component={NotFoundPage} />
+        </Switch>
+      </Suspense>
     </BrowserRouter>
   )
 }
