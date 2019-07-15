2df4770e2ea14963e7410ad402373807e4dd06c0
Bruno Valenga <brunodasilvalenga@Bruno-Valenga-iMac.local>
2019-07-16 09:59:59 +1000

    create page

--- src/pages/Listing/SpacePage/AvailabilityTab/index.js
+++ src/pages/Listing/SpacePage/AvailabilityTab/index.js
@@ -1,11 +1,28 @@
 import React from 'react'
-import { Wrapper } from 'components'
+import { Title, Grid, Cell, TimeTable } from 'components'
 
 const AvailabilityTab = () => {
   return (
-    <Wrapper>
-      <div />
-    </Wrapper>
+    <Grid columns={1} rowGap="80px">
+      <Cell>
+        <Title type="h3" title="Timetable*" subtitle="Let guests know the times your space is open." />
+        <TimeTable />
+      </Cell>
+      <Cell>
+        <Title
+          type="h3"
+          title="Blocked dates"
+          subtitle="Block out times when the space is not available within business opening hours."
+        />
+      </Cell>
+      <Cell>
+        <Title
+          type="h3"
+          title="Holidays"
+          subtitle="Are you closed on all Australian holidays? Or Just a few of them?"
+        />
+      </Cell>
+    </Grid>
   )
 }
 
