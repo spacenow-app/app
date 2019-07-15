71cd9704fa53317c17e5dc8ab6d252a513db03e0
Bruno Valenga <brunodasilvalenga@Bruno-Valenga-iMac.local>
2019-07-16 09:58:23 +1000

    remove default component import

--- src/components/Grid/Grid.js
+++ src/components/Grid/Grid.js
@@ -1,22 +1,44 @@
-import React from 'react'
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
-import { Grid as GridImported } from 'styled-css-grid'
 
-const GridStyled = styled(GridImported)`
-  width: ${props => props.width};
+const autoRows = ({ minRowHeight = '20px' }) => `minmax(${minRowHeight}, auto)`
+
+const frGetter = value => (typeof value === 'number' ? `repeat(${value}, 1fr)` : value)
+
+const gap = ({ gapValue = '8px' }) => gapValue
+
+const flow = ({ flowValue = 'row' }) => flowValue
+
+const formatAreas = areas => areas.map(area => `"${area}"`).join(' ')
+
+const Grid = styled.div`
+  display: grid;
+  height: ${({ height = 'auto' }) => height};
+  grid-auto-flow: ${flow};
+  grid-auto-rows: ${autoRows};
+  ${({ rows }) => rows && `grid-template-rows: ${frGetter(rows)}`};
+  grid-template-columns: ${({ columns = 12 }) => frGetter(columns)};
+  grid-gap: ${gap};
+  ${({ columnGap }) => columnGap && `column-gap: ${columnGap}`};
+  ${({ rowGap }) => rowGap && `row-gap: ${rowGap}`};
+  ${({ areas }) => areas && `grid-template-areas: ${formatAreas(areas)}`};
+  ${({ justifyContent }) => justifyContent && `justify-content: ${justifyContent}`};
+  ${({ alignContent }) => alignContent && `align-content: ${alignContent}`};
 `
 
-const Grid = ({ children, ...props }) => {
-  return <GridStyled {...props}>{children}</GridStyled>
-}
-
-Grid.defaultProps = {
-  children: null
-}
-
 Grid.propTypes = {
-  children: PropTypes.element
+  className: PropTypes.string,
+  columns: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  gap: PropTypes.string,
+  columnGap: PropTypes.string,
+  rowGap: PropTypes.string,
+  height: PropTypes.string,
+  minRowHeight: PropTypes.string,
+  flow: PropTypes.string,
+  rows: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  areas: PropTypes.arrayOf(PropTypes.string),
+  justifyContent: PropTypes.string,
+  alignContent: PropTypes.string
 }
 
 export default Grid
