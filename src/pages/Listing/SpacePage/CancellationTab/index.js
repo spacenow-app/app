087327fffee09028c885980403596dd0e335ee7b
Bruno Valenga <brunodasilvalenga@Bruno-Valenga-iMac.local>
2019-07-16 09:59:55 +1000

    create page

--- src/pages/Listing/SpacePage/CancellationTab/index.js
+++ src/pages/Listing/SpacePage/CancellationTab/index.js
@@ -1,10 +1,37 @@
-import React, { Component } from 'react'
-import { Wrapper } from 'components'
+import React from 'react'
+import styled from 'styled-components'
+import { Title, Select, Input, Caption, Radio, Grid, Cell } from 'components'
 
-export class CancellationTab extends Component {
-  render() {
-    return <Wrapper>CancellationTab</Wrapper>
-  }
+import GraphCancelattionImage from './graph_cancellation.png'
+
+const ImageStyled = styled.img`
+  width: 100%;
+`
+
+const CancellationTab = () => {
+  return (
+    <Grid columns={1}>
+      <Cell>
+        <Title type="h3" title="Cancellation Policy" />
+      </Cell>
+      <Cell>
+        <Grid columns={12}>
+          <Cell width={4}>
+            <Title
+              noMargin
+              type="h4"
+              title="No Cancellation"
+              subTitleSize={16}
+              subtitle="Guest cannot cancel their booking. Note: This may affect the number of bookings received."
+            />
+          </Cell>
+          <Cell width={8}>
+            <ImageStyled alt="Cancellation Policy" src={GraphCancelattionImage} />
+          </Cell>
+        </Grid>
+      </Cell>
+    </Grid>
+  )
 }
 
 export default CancellationTab
