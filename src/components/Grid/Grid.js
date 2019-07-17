f0de8fd1483fbd18e42ef8e714e983ab6f770764
Bruno Valenga <brunodasilvalenga@Bruno-Valenga-iMac.local>
2019-07-17 16:34:29 +1000

    remake functions name

--- src/components/Grid/Grid.js
+++ src/components/Grid/Grid.js
@@ -5,20 +5,20 @@ const autoRows = ({ minRowHeight = '20px' }) => `minmax(${minRowHeight}, auto)`
 
 const frGetter = value => (typeof value === 'number' ? `repeat(${value}, 1fr)` : value)
 
-const gap = ({ gapValue = '8px' }) => gapValue
+const gapValue = ({ gap = '8px' }) => gap
 
-const flow = ({ flowValue = 'row' }) => flowValue
+const flowValue = ({ flow = 'row' }) => flow
 
 const formatAreas = areas => areas.map(area => `"${area}"`).join(' ')
 
 const Grid = styled.div`
   display: grid;
   height: ${({ height = 'auto' }) => height};
-  grid-auto-flow: ${flow};
+  grid-auto-flow: ${flowValue};
   grid-auto-rows: ${autoRows};
   ${({ rows }) => rows && `grid-template-rows: ${frGetter(rows)}`};
   grid-template-columns: ${({ columns = 12 }) => frGetter(columns)};
-  grid-gap: ${gap};
+  grid-gap: ${gapValue};
   ${({ columnGap }) => columnGap && `column-gap: ${columnGap}`};
   ${({ rowGap }) => rowGap && `row-gap: ${rowGap}`};
   ${({ areas }) => areas && `grid-template-areas: ${formatAreas(areas)}`};
