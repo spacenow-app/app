b01a2140a9d01808cf60edd8c7320ba91f08519b
Bruno Valenga <brunodasilvalenga@Bruno-Valenga-iMac.local>
2019-07-19 17:08:52 +1000

    create new away to styles

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import React from 'react'
 import { Provider } from 'react-redux'
+import { ThemeProvider } from 'styled-components'
 import { store } from './redux/store'
 import Routes from './routes'
-import { GlobalStyle } from './styles'
+import { global as GlobalStyle, theme } from './styles'
 
 const App = () => {
   return (
     <Provider store={store}>
-      <GlobalStyle />
-      <Routes />
+      <ThemeProvider theme={theme}>
+        <>
+          <GlobalStyle />
+          <Routes />
+        </>
+      </ThemeProvider>
     </Provider>
   )
 }
