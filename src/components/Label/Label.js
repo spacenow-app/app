eb301e3acbceabf728181b7ba6c72270d847b0bc
Bruno Valenga <brunodasilvalenga@Bruno-Valenga-iMac.local>
2019-07-19 17:09:01 +1000

    create new component

--- src/components/Label/Label.js
+++ src/components/Label/Label.js
@@ -1,75 +1,42 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
+import { color, border } from 'styled-system'
+import propTypes from '@styled-system/prop-types'
 
 const LabelStyled = styled.span`
-  min-width: ${props => props.theme.label.minWidth};
-  align-self: ${props => props.align};
-  justify-self: ${props => props.justify};
+  min-width: 125px;
   height: min-content;
   width: max-content;
-  padding: ${props => props.theme.label.padding};
-  background-color: ${props => props.theme.label.pending.backgroundColor};
-  color: ${props => props.theme.label.pending.textColor};
-  border-radius: ${props => props.theme.label.borderRadius};
+  padding: 10px;
   text-align: center;
   font-weight: 700;
-  &.active {
-    background-color: ${props => props.theme.label.active.backgroundColor};
-    color: ${props => props.theme.label.active.textColor};
-  }
-  &.expired {
-    background-color: ${props => props.theme.label.expired.backgroundColor};
-    color: ${props => props.theme.label.expired.textColor};
-  }
-  &.approved {
-    background-color: ${props => props.theme.label.completed.backgroundColor};
-    color: ${props => props.theme.label.completed.textColor};
-  }
-  &.requested {
-    background-color: ${props => props.theme.label.completed.backgroundColor};
-    color: ${props => props.theme.label.completed.textColor};
-  }
-  &.completed {
-    background-color: ${props => props.theme.label.completed.backgroundColor};
-    color: ${props => props.theme.label.completed.textColor};
-  }
-  &.cancelled {
-    background-color: ${props => props.theme.label.cancelled.backgroundColor};
-    color: ${props => props.theme.label.cancelled.textColor};
-  }
+  align-self: ${props => props.align};
+  justify-self: ${props => props.justify};
+  ${color}
+  ${border}
 `
 
-const Label = props => (
-  <LabelStyled
-    rounded={props.rounded}
-    className={props.className}
-    backgroundColor={props.backgroundColor}
-    align={props.align}
-    justify={props.justify}
-  >
-    {props.children}
+const Label = ({ children, align, justify, ...props }) => (
+  <LabelStyled {...props} align={align} justify={justify}>
+    {children}
   </LabelStyled>
 )
 
 Label.defaultProps = {
-  rounded: false,
-  children: null,
-  className: null,
-  backgroundColor: null,
   align: 'center',
-  justify: 'center'
+  justify: 'center',
+  bg: 'greyLight',
+  color: 'greyscale.2',
+  borderRadius: '8px'
 }
 
 Label.propTypes = {
-  // eslint-disable-next-line react/no-unused-prop-types
-  theme: PropTypes.instanceOf(Object).isRequired,
-  rounded: PropTypes.bool,
   children: PropTypes.string.isRequired,
-  className: PropTypes.string,
-  backgroundColor: PropTypes.string,
   align: PropTypes.string,
-  justify: PropTypes.string
+  justify: PropTypes.string,
+  ...propTypes.color,
+  ...propTypes.border
 }
 
 export default Label
