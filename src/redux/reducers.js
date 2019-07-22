15804732a4e7b114bb53dcb7924bf69ff5620d30
Arthemus <arthemus.moreira@gmail.com>
2019-07-22 14:13:48 +1000

    Fetch all categories from database.

--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -4,10 +4,12 @@ import modal from './ducks/modal'
 import auth from './ducks/auth'
 import system from './ducks/system'
 import location from './ducks/location'
+import category from './ducks/category'
 
 export default combineReducers({
   modal,
   system,
   auth,
-  location
+  location,
+  category
 })
