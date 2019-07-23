239e6c0200c89225250fd195862b04d2e1b3b2ee
Bruno Valenga <brunodasilvalenga@Bruno-Valenga-iMac.local>
2019-07-23 15:00:00 +1000

    create new reducres listing

--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -4,10 +4,12 @@ import modal from './ducks/modal'
 import auth from './ducks/auth'
 import system from './ducks/system'
 import location from './ducks/location'
+import listing from './ducks/listing'
 
 export default combineReducers({
   modal,
   system,
   auth,
-  location
+  location,
+  listing
 })
