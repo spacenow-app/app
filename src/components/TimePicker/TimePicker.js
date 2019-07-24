0278a1ed049e05c91405ecd3dbbeca6cb118c049
Bruno Valenga <brunodasilvalenga@gmail.com>
2019-07-24 15:39:55 +1000

    change time picker

--- src/components/TimePicker/TimePicker.js
+++ src/components/TimePicker/TimePicker.js
@@ -1,28 +1,47 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import TimePickerImported from 'rc-time-picker'
-import './rc-time-picker.css'
+import styled from 'styled-components'
+import TimeField from 'react-simple-timefield'
 
-const TimePicker = props => {
-  return (
-    <TimePickerImported
-      {...props}
-      use12Hours
-      hideDisabledOptions
-      minuteStep={5}
-      showSecond={false}
-      placeholder="00:00 am"
-      allowEmpty={false}
-    />
-  )
+const InputStyled = styled.input`
+  text-align: center;
+  width: 100%;
+  position: relative;
+  display: inline-block;
+  /* padding: 4px 7px; */
+  height: 28px;
+  cursor: text;
+  font-size: 16px;
+  line-height: 1.5;
+  color: #172439;
+  background-color: #fff;
+  background-image: none;
+  border: none;
+  transition: border 0.2s cubic-bezier(0.645, 0.045, 0.355, 1), background 0.2s cubic-bezier(0.645, 0.045, 0.355, 1),
+    box-shadow 0.2s cubic-bezier(0.645, 0.045, 0.355, 1);
+
+  &:disabled {
+    color: #ccc;
+    cursor: not-allowed;
+  }
+
+  :focus {
+    outline: none;
+  }
+`
+
+const TimePicker = ({ value, disabled, ...props }) => {
+  return <TimeField {...props} value={value} input={<InputStyled />} disabled={disabled} />
 }
 
 TimePicker.defaultProps = {
-  children: null
+  value: null,
+  disabled: false
 }
 
 TimePicker.propTypes = {
-  children: PropTypes.element
+  value: PropTypes.string,
+  disabled: PropTypes.bool
 }
 
 export default TimePicker
