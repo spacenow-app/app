3bbb3465ce29b9e907977b6fc554bdfa73e23ee3
Bruno Valenga <brunodasilvalenga@gmail.com>
2019-07-25 16:02:35 +1000

    remove 24h

--- src/components/TimeTable/TimeTableEditable.js
+++ src/components/TimeTable/TimeTableEditable.js
@@ -69,7 +69,7 @@ const TimeTableEditable = ({
 
   return (
     <WrapperStyled>
-      <Grid columns={12} columnGap="40px" style={{ margin: '30px 0' }}>
+      {/* <Grid columns={12} columnGap="40px" style={{ margin: '30px 0' }}>
         <Cell width={4}>
           <ItemSwitchStyled>
             <span>Open 24 / 7</span>
@@ -78,7 +78,7 @@ const TimeTableEditable = ({
             </SwitchStyled>
           </ItemSwitchStyled>
         </Cell>
-      </Grid>
+      </Grid> */}
       <Grid columns={12} columnGap="40px" left>
         <Cell width={4} middle>
           <TitleStyled>Days</TitleStyled>
