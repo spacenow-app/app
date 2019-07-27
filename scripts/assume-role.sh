dcaddb88504f8bef2ddbd7fbd4c61b98744c63a4
Lucas Nascimento <csalucasnascimento@gmail.com>
2019-07-27 11:04:28 +1000

    test

--- scripts/assume-role.sh
+++ scripts/assume-role.sh
@@ -6,7 +6,7 @@ temp_role=$(aws sts assume-role \
   --role-session-name "spacenow-cli" \
   )
 
-echo "export AWS_ACCESS_KEY_ID=$(echo $temp_role | jq .Credentials.AccessKeyId | xargs)" >> $BAH_ENV
-echo "export AWS_SECRET_ACCESS_KEY=$(echo $temp_role | jq .Credentials.SecretAccessKey | xargs)" >> $BAH_ENV
-echo "export AWS_SESSION_TOKEN=$(echo $temp_role | jq .Credentials.SessionToken | xargs)" >> $BAH_ENV
+echo "export AWS_ACCESS_KEY_ID=$(echo $temp_role | jq .Credentials.AccessKeyId | xargs)" >> $BASH_ENV
+echo "export AWS_SECRET_ACCESS_KEY=$(echo $temp_role | jq .Credentials.SecretAccessKey | xargs)" >> $BASH_ENV
+echo "export AWS_SESSION_TOKEN=$(echo $temp_role | jq .Credentials.SessionToken | xargs)" >> $BASH_ENV
 
