5347459ebd8d3fb5229472e4440eecfe099809ba
Arthemus <arthemus.moreira@gmail.com>
2019-08-09 10:24:42 +1000

    Action to remove account.

--- src/pages/Account/PaymentPage/index.js
+++ src/pages/Account/PaymentPage/index.js
@@ -1,7 +1,11 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React, { useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
 import styled from 'styled-components'
-import { Title, Radio, Icon, Box } from 'components'
+import Helmet from 'react-helmet'
+
+import { onGetPaymentAccount, onDeletePaymentAccount } from 'redux/ducks/payment'
+
+import { Wrapper, Title, Icon, Loader } from 'components'
 
 const ListGroup = styled.div`
   margin: 50px;
@@ -11,59 +15,51 @@ const ListGroup = styled.div`
 
 const ListItem = styled.div`
   display: grid;
-  grid-template-columns: auto 1fr 1fr 1fr auto;
+  grid-template-columns: 1fr 1fr 1fr auto;
   padding: 15px;
   border: 1px solid #ececec;
   border-radius: 15px;
-  align-items: center;
 `
 
 const AccountName = styled.span``
-const BSB = styled.span``
-const AccountNumber = styled.span``
-const Options = styled.div`
-  display: grid;
-  grid-template-columns: auto auto;
-  grid-column-gap: 15px;
-`
 
-const PaymentPage = ({ ...props }) => {
+const BSB = styled.span``
+
+const AccountNumber = styled.span``
+
+const PaymentPage = () => {
+  const dispatch = useDispatch()
+
+  const { isLoading } = useSelector(state => state.payment)
+  const { object: details } = useSelector(state => state.payment.get)
+
+  useEffect(() => {
+    dispatch(onGetPaymentAccount())
+  }, [dispatch])
+
+  const _handleRemoveAccount = () => {
+    dispatch(onDeletePaymentAccount())
+  }
+
+  if (isLoading) return <Loader text="Loading listing process" />
+
+  if (!details) return <Loader text="Loading listing process" /> // TODO Render form...
+
   return (
-    <Box>
-      <Title type="h3" title="Payment Methods" subtitle="lorem ipsom" />
+    <Wrapper>
+      <Helmet title="Payment Preferences - Spacenow" />
+      <Title type="h3" title="Payment Preferences" />
       <ListGroup>
         <ListItem>
-          <Radio />
-          <AccountName>Bruno da Silva Valenga</AccountName>
-          <BSB>000-123</BSB>
-          <AccountNumber>1233123</AccountNumber>
-          <Options>
-            <Icon width="25px" />
-            <Icon width="25px" />
-          </Options>
-        </ListItem>
-        <ListItem>
-          <Radio />
-          <AccountName>Bruno da Silva Valenga</AccountName>
-          <BSB>000-123</BSB>
-          <AccountNumber>1233123</AccountNumber>
-          <Options>
-            <Icon width="25px" />
-            <Icon width="25px" />
-          </Options>
-        </ListItem>
-        <ListItem>
-          <Radio />
-          <AccountName>Bruno da Silva Valenga</AccountName>
-          <BSB>000-123</BSB>
-          <AccountNumber>1233123</AccountNumber>
-          <Options>
-            <Icon width="25px" />
-            <Icon width="25px" />
-          </Options>
+          <AccountName>{details.legal_entity.first_name}</AccountName>
+          <BSB>{details.external_accounts.data[0].routing_number}</BSB>
+          <AccountNumber>{details.external_accounts.data[0].last4}</AccountNumber>
+          <a href onClick={_handleRemoveAccount}>
+            <Icon name="bin" width="25px" />
+          </a>
         </ListItem>
       </ListGroup>
-    </Box>
+    </Wrapper>
   )
 }
 
