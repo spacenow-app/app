ed7dded43fd32dad2021ab64b97cfafc49001c95
Bruno <brunodasilvalenga@gmail.com>
2019-08-12 17:52:16 +1000

    edit recipts on mail

--- src/redux/ducks/mail.js
+++ src/redux/ducks/mail.js
@@ -64,7 +64,7 @@ export const sendMailForm = values => async dispatch => {
 
   try {
     const emailOptions = {
-      to: 'bruno@spacenow.com',
+      to: 'barrett@spacenow.com, team@spacenow.com',
       subject: 'Spacenow Landing Page - Lead Requirements Form ',
       html: `
         <html>
