b68233699caabe285efa9c1e116870a28b4c12fe
Camila <kmi_218@hotmail.com>
2019-08-14 10:11:23 +1000

    Add missing pages to index

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,4 +4,4 @@ import { IntroPage, LocationPage, CategoryPage, SpacePage } from './Listing'
 import { ProfilePage, PaymentPage } from './Account'
 import { PartnerPage } from './Space'
 
-export { HomePage, NotFoundPage, IntroPage, LocationPage, CategoryPage, SpacePage, PartnerPage }
+export { HomePage, NotFoundPage, IntroPage, LocationPage, CategoryPage, SpacePage, PartnerPage, ProfilePage, PaymentPage }
