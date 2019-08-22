a073f99fc44335c238db9bcd3ae7042b78bf5f60
Bruno <brunodasilvalenga@gmail.com>
2019-08-22 16:42:49 +1000

    create new link component

--- src/components/Link/Link.js
+++ src/components/Link/Link.js
@@ -1,100 +1,21 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { Link as LinkRouter } from 'react-router-dom'
 import styled from 'styled-components'
+import { color } from 'styled-system'
 
-const LinkStyled = styled.div`
-  cursor: ${props => (props.disabled ? 'not-allowed' : 'pointer')};
-  border-radius: ${props => props.theme.link.baseRadius};
-  font-size: ${props => {
-    const { baseFontSize } = props.theme.link
-    const baseFontSizeParsed = parseInt(baseFontSize, 10)
-    return (
-      (props.small && `${baseFontSizeParsed * 0.875}px`) ||
-      (props.large && `${baseFontSizeParsed * 1.375}px`) ||
-      baseFontSize
-    )
-  }};
-  font-weight: ${props => props.theme.link.fontSemibold};
-  line-height: ${props => (props.small && '2.2') || (props.large && '1.25') || '2.5'};
-  padding: ${props => (props.large ? '16px 25px 17px' : '0 12px')};
-  display: grid;
-  justify-content: ${props => props.align};
-  aling-items: center;
-  padding: 6px 12px;
-  margin-bottom: 0;
-  line-height: 1.42857143;
-  white-space: nowrap;
-  vertical-align: middle;
-  touch-action: manipulation;
-  user-select: none;
-  outline-offset: -2px;
-  > a {
-    color: ${props =>
-      props.disabled ? props.theme.link.default.inactive.textColor : props.theme.link.default.active.textColor};
-    cursor: ${props => (props.disabled ? 'not-allowed' : 'pointer')};
-    text-decoration: none;
-  }
+const Link = styled(LinkRouter)`
+  ${color}
+
   :hover {
-    background-color: ${props => (props.disabled ? 'transparent' : props.theme.link.default.hover.backgroundColor)};
+    ${color}
   }
 `
 
-const isLeftClickEvent = event => event.button === 0
-const isModifiedEvent = event => !!(event.metaKey || event.altKey || event.ctrlKey || event.shiftKey)
-
-const Link = props => {
-  const handleClick = event => {
-    if (props.disabled) {
-      event.preventDefault()
-      return
-    }
-    if (isModifiedEvent(event) || !isLeftClickEvent(event)) {
-      return
-    }
-    if (event.defaultPrevented === true) {
-      return
-    }
-    if (props.onClick) {
-      props.onClick(event)
-    }
-  }
-
-  return (
-    <LinkStyled
-      small={props.small}
-      large={props.large}
-      className={props.className}
-      disabled={props.disabled}
-      align={props.align}
-    >
-      <a href={props.to} {...props} onClick={handleClick}>
-        {props.children}
-      </a>
-    </LinkStyled>
-  )
-}
-
 Link.defaultProps = {
-  disabled: false,
-  className: null,
-  small: false,
-  large: false,
-  to: null,
-  onClick: null,
-  align: 'left'
+  color: 'secondary'
 }
 
-Link.propTypes = {
-  children: PropTypes.string.isRequired,
-  // eslint-disable-next-line react/no-unused-prop-types
-  theme: PropTypes.instanceOf(Object).isRequired,
-  onClick: PropTypes.func,
-  disabled: PropTypes.bool,
-  className: PropTypes.string,
-  small: PropTypes.bool,
-  large: PropTypes.bool,
-  to: PropTypes.string,
-  align: PropTypes.string
-}
+Link.propTypes = {}
 
 export default Link
