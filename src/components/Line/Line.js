d270f00fa09bd1732187da1ebe7aa99ae9e7d5fb
Bruno <brunodasilvalenga@gmail.com>
2019-08-22 16:42:42 +1000

    create new component hr

---
+++ src/components/Line/Line.js
@@ -0,0 +1,13 @@
+import PropTypes from 'prop-types'
+import styled from 'styled-components'
+import { space } from 'styled-system'
+
+const Line = styled.hr`
+  ${space}
+`
+
+Line.defaultProps = {}
+
+Line.propTypes = {}
+
+export default Line
