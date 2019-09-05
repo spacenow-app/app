ac1270d386d0158a5f107f5668ecad8d2a45e7b9
Bruno <brunodasilvalenga@gmail.com>
2019-09-05 10:58:10 +1000

    include not found page

--- src/routes/Authentication/index.js
+++ src/routes/Authentication/index.js
@@ -1,16 +1,19 @@
 import React, { Suspense } from 'react'
 import PropTypes from 'prop-types'
-import { Route } from 'react-router-dom'
+import { Switch, Route, Redirect } from 'react-router-dom'
 import { Loader } from 'components'
-import { SigninPage, SignupPage, ForgotPasswordPage, ResetPasswordPage } from 'pages'
+import { NotFoundPage, SigninPage, SignupPage, ForgotPasswordPage, ResetPasswordPage } from 'pages'
 
 const Landingpages = ({ match, ...props }) => {
   return (
     <Suspense fallback={<Loader />}>
-      <Route exact component={SigninPage} path={`${match.path}/signin`} />
-      <Route exact component={SignupPage} path={`${match.path}/signup`} />
-      <Route exact component={ForgotPasswordPage} path={`${match.path}/forgot_password`} />
-      <Route exact component={ResetPasswordPage} path={`${match.path}/reset_password`} />
+      <Switch>
+        <Route exact component={SigninPage} path={`${match.path}/signin`} />
+        <Route exact component={SignupPage} path={`${match.path}/signup`} />
+        <Route exact component={ForgotPasswordPage} path={`${match.path}/forgot_password`} />
+        <Route exact component={ResetPasswordPage} path={`${match.path}/reset_password`} />
+        <Redirect to="/404" />
+      </Switch>
     </Suspense>
   )
 }
