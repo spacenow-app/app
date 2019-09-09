12a51602a7b03dde3f250364a9e3821a1aaf747b
Camila <kmi_218@hotmail.com>
2019-09-09 16:19:09 +1000

    Start checkout page structure

--- src/pages/CheckoutPage/index.js
+++ src/pages/CheckoutPage/index.js
@@ -7,66 +7,21 @@ import Helmet from 'react-helmet'
 import { 
   // useSelector, 
   useDispatch } from 'react-redux'
-// import styled from 'styled-components'
-// import _ from 'lodash'
-// import { isSameDay, format } from 'date-fns'
-
-// import { capitalize, toPlural } from 'utils/strings'
 
 import {
   Wrapper,
   Title,
   Grid,
   Cell,
-  // TimeTable,
-  // Box,
-  // Icon,
-  // Loader,
   UserDetails,
   BookingCard,
-  // Checkbox,
-  // Button
 } from 'components'
 
-// import {
-//   checkBookingState
-// } from 'redux/ducks/checkout'
-
-// import { openModal, TypesModal } from 'redux/ducks/modal'
-
-// import config from 'variables/config'
 
 const CheckoutPage = ({ match, location, ...props }) => {
   const dispatch = useDispatch()
 
-  // const { object: listing, isLoading: isListingLoading } = useSelector(state => state.listing.get)
-
-  // const [datesSelected, setDatesSelected] = useState([])
-
   useEffect(() => {
-
-    // dispatch(checkBookingState(match.params.id, ['approved', 'requested']))
-    // .catch(() => history.replace('/dashboard'))
-    // .then(isDone => {
-    //   console.log('isDone', isDone)
-      // if (isDone) {
-      //   toastr.info('Information', 'Reservation is already paid.')
-      //   history.replace(`/itinerary/${reservationCode}`)
-      // } else {
-      //   this.props
-      //     .checkBookingState(reservationCode, ['timeout'])
-      //     .catch(() => history.replace('/dashboard/bookings'))
-      //     .then(isDone => {
-      //       if (isDone) {
-      //         toastr.info('Information', 'Reservation is cancelled.')
-      //         history.replace('/dashboard/bookings')
-      //       } else {
-      //         this.props.loadReservation(reservationCode)
-      //         this.props.getCard()
-      //       }
-      //     })
-      // }
-    // })
   }, [dispatch, match.params.id])
 
   return (
@@ -74,7 +29,7 @@ const CheckoutPage = ({ match, location, ...props }) => {
       <Helmet title="Checkout - Spacenow" />
       <Grid columns="auto 350px" columnGap="15px" rowGap="100px">
         <Cell> 
-
+          
         </Cell>
         <Cell> 
           <BookingCard
--- src/redux/ducks/checkout.js
+++ src/redux/ducks/checkout.js
@@ -1,55 +1,110 @@
-// /* eslint-disable no-console */
-// import { gql } from 'apollo-boost'
+/* eslint-disable no-console */
+import { gql } from 'apollo-boost'
 
-// import { getClientWithAuth } from 'graphql/apolloClient'
-// import errToMsg from 'utils/errToMsg'
-// import { config } from 'variables'
+import { getClientWithAuth } from 'graphql/apolloClient'
+import errToMsg from 'utils/errToMsg'
+import { config } from 'variables'
 
-// // Actions
-// export const Types = {
-// }
+// Actions
+export const Types = {
+  GET_BOOKING_REQUEST = 'GET_BOOKING_REQUEST',
+  GET_BOOKING_SUCCESS = 'GET_BOOKING_SUCCESS',
+  GET_BOOKING_FAILURE = 'GET_BOOKING_FAILURE'
+}
 
-// // Initial State
-// const initialState = {
-//   isLoading: false,
-// }
+// Initial State
+const initialState = {
+  isLoading: false,
+  reservationData: {},
+}
 
-// const queryGetBookingState = gql`
-//   query GetBookingState($bookingId: String!) {
-//     GetBookingById(bookingId: $bookingId) {
-//       bookingId
-//       bookingState
-//     }
-//   }
-// `;
+const queryGetBookingState = gql`
+  query getBookingState($bookingId: String!) {
+    getBooking(bookingId: $bookingId) {
+      bookingId
+      bookingState
+    }
+  }
+`;
 
-// const queryGetBookingById = gql`
-//   query GetBookingById($bookingId: String!) {
-//     GetBookingById(bookingId: $bookingId) {
-//       bookingId
-//       quantity
-//       currency
-//       totalPrice
-//       listingId
-//       totalDays
-//       basePrice
-//       createdAt
-//       sourceId
-//       chargeId
-//       hostServiceFee
-//       bookingState
-//       confirmationCode
-//       bookingId
-//       guestServiceFee
-//       fees
-//       hostId
-//       paymentState
-//       updatedAt
-//       priceType
-//       guestId
-//       period
-//       reservations
-//     }
-//   }
-// `;
+const queryGetBooking = gql`
+  query getBooking($bookingId: String!) {
+    getBooking(bookingId: $bookingId) {
+      bookingId
+      quantity
+      currency
+      totalPrice
+      listingId
+      totalDays
+      basePrice
+      createdAt
+      sourceId
+      chargeId
+      hostServiceFee
+      bookingState
+      confirmationCode
+      bookingId
+      guestServiceFee
+      fees
+      hostId
+      paymentState
+      updatedAt
+      priceType
+      guestId
+      period
+      reservations
+    }
+  }
+`;
 
+
+export default function reducer(state = initialState, action) {
+  switch (action.type) {
+    case Types.GET_BOOKING_REQUEST: {
+      return {
+        ...state,
+        create: {
+          isLoading: true,
+          error: null
+        }
+      }
+    }
+    case Types.GET_BOOKING_SUCCESS: {
+      return {
+        ...state,
+        create: {
+          isLoading: false,
+          object: action.payload
+        }
+      }
+    }
+    case Types.GET_BOOKING_FAILURE: {
+      return {
+        ...state,
+        create: {
+          isLoading: false,
+          error: action.payload
+        }
+      }
+    }
+    default:
+      return state
+  }
+}
+
+
+// Action Creators
+
+// Side Effects
+export const onGetBooking = bookingId => async dispatch => {
+  dispatch({ type: Types.GET_BOOKING_REQUEST })
+  try {
+    const { data } = await getClientWithAuth(dispatch).mutate({
+      mutation: queryGetBooking,
+      variables: bookingId
+    })
+    dispatch({ type: Types.GET_BOOKING_SUCCESS, payload: data.getBooking })
+  } catch (err) {
+    dispatch({ type: Types.GET_BOOKING_FAILURE, payload: errToMsg(err) })
+  }
+}
