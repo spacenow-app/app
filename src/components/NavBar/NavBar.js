8f86a3db1fb050a375b198f3233ff494eb3363b3
Bruno <brunodasilvalenga@gmail.com>
2019-09-12 10:26:59 +1000

    change navbar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { Link } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 import styled from 'styled-components'
 import { Navbar, Nav, NavDropdown } from 'react-bootstrap'
 import { Avatar, Box } from 'components'
@@ -18,29 +18,82 @@ const NavDropdownStyled = styled(NavDropdown)`
   }
 `
 
-function NavBar() {
+const DropdownItemStyled = styled(Link)`
+  display: block;
+  width: 100%;
+  padding: 0.25rem 1.5rem;
+  clear: both;
+  font-weight: 400;
+  color: #172439;
+  text-align: inherit;
+  white-space: nowrap;
+  background-color: transparent;
+  border: 0;
+  text-decoration: none;
+
+  :hover {
+    color: #172439;
+    text-decoration: none;
+    background-color: #f8f9fa;
+  }
+
+  :active {
+    color: #fff;
+    text-decoration: none;
+    background-color: #51c482;
+  }
+`
+
+const NavLinkStyled = styled(NavLink)`
+  display: block;
+  padding: 0.5rem 1rem;
+  padding-right: 0.5rem;
+  padding-left: 0.5rem;
+  color: #172439;
+  text-decoration: none;
+
+  :hover {
+    color: #51c482;
+    text-decoration: none;
+  }
+`
+
+const NavBar = () => {
   const dispatch = useDispatch()
   const { user } = useSelector(state => state.account.get)
   const { isAuthenticated } = useSelector(state => state.auth)
 
-  const _handlerGoToLegancy = () => {
-    window.location.href = `${config.legacy}`
+  const _handlerGoToLegancy = page => () => {
+    window.location.href = `${config.static}/${page || ''}`
   }
-  const _handlerLogout = () => {
+  const _handlerLogout = () => () => {
     dispatch(logout())
   }
 
   return (
-    <Navbar>
+    <Navbar expand="lg">
       <Link to="" onClick={_handlerGoToLegancy}>
         <Navbar.Brand>
           <img alt="" src={logo} width={230} className="d-inline-block align-top" />
         </Navbar.Brand>
       </Link>
-      <Navbar.Toggle />
-      {isAuthenticated && (
-        <Navbar.Collapse className="justify-content-end">
-          <Nav>
+      <Navbar.Toggle aria-controls="top-navbar-nav" />
+      <Navbar.Collapse className="justify-content-end" id="top-navbar-nav">
+        <Nav style={{ alignItems: 'center' }}>
+          <NavLinkStyled to="/listing">List Your Space</NavLinkStyled>
+          <NavLinkStyled to="" onClick={_handlerGoToLegancy('help')}>
+            Help
+          </NavLinkStyled>
+          {!isAuthenticated ? (
+            <>
+              <NavLinkStyled to="/auth/signin" href="#link">
+                Sign In
+              </NavLinkStyled>
+              <NavLinkStyled to="/auth/signup" href="#link">
+                Sign Up
+              </NavLinkStyled>
+            </>
+          ) : (
             <NavDropdownStyled
               alignRight
               title={
@@ -51,15 +104,15 @@ function NavBar() {
               }
               id="basic-nav-dropdown"
             >
-              <NavDropdown.Item href={`/account/profile`}>Profile</NavDropdown.Item>
+              <DropdownItemStyled to="/account/profile">Profile</DropdownItemStyled>
               <NavDropdown.Divider />
-              <NavDropdown.Item href={`/account/dashboard`}>Dashboard</NavDropdown.Item>
+              <DropdownItemStyled href="/account/dashboard">Dashboard</DropdownItemStyled>
               <NavDropdown.Divider />
-              <NavDropdown.Item onClick={_handlerLogout}>Logout</NavDropdown.Item>
+              <DropdownItemStyled onClick={_handlerLogout}>Logout</DropdownItemStyled>
             </NavDropdownStyled>
-          </Nav>
-        </Navbar.Collapse>
-      )}
+          )}
+        </Nav>
+      </Navbar.Collapse>
     </Navbar>
   )
 }
