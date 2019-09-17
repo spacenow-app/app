4d09e51796757bab9bf2bfe9052e7a9f89205a37
Lucas <csalucasnascimento@gmail.com>
2019-09-17 12:28:46 +1000

    align center

--- src/pages/CheckoutPage/index.js
+++ src/pages/CheckoutPage/index.js
@@ -164,9 +164,9 @@ const CheckoutPage = ({ match, location, history, ...props }) => {
               <ListDates dates={reservation.reservations} />
             </>
           ) : (
-            // <BookingDates reservationData={[]} />
-            <></>
-          )}
+              // <BookingDates reservationData={[]} />
+              <></>
+            )}
 
           <TimeTable data={listing.accessDays.listingAccessHours} />
 
@@ -180,10 +180,10 @@ const CheckoutPage = ({ match, location, history, ...props }) => {
                 <thead>
                   <tr>
                     <th />
-                    <th>Name on Card</th>
-                    <th>Brand</th>
-                    <th>Card Number</th>
-                    <th>Options</th>
+                    <th><Text fontSize="14px" fontFamily="semiBold" style={{ whiteSpace: "nowrap" }}>Name on Card</Text></th>
+                    <th><Text fontSize="14px" fontFamily="semiBold" style={{ whiteSpace: "nowrap" }}>Brand</Text></th>
+                    <th><Text fontSize="14px" fontFamily="semiBold" style={{ whiteSpace: "nowrap" }}>Card Number</Text></th>
+                    <th><Text fontSize="14px" fontFamily="semiBold" style={{ whiteSpace: "nowrap" }}>Options</Text></th>
                   </tr>
                 </thead>
                 <tbody>
@@ -192,17 +192,17 @@ const CheckoutPage = ({ match, location, history, ...props }) => {
                       <td>
                         <Checkbox onClick={_handleChangeCardSelect(card)} checked={selectedCard.id === card.id} />
                       </td>
-                      <td>{card.name}</td>
-                      <td>{card.brand}</td>
-                      <td>{`**** **** **** ${card.last4}`}</td>
-                      <td>
+                      <td><Text fontSize="14px" style={{ whiteSpace: "nowrap" }}>{card.name}</Text></td>
+                      <td><Text fontSize="14px" style={{ whiteSpace: "nowrap" }}>{card.brand}</Text></td>
+                      <td><Text fontSize="14px" style={{ whiteSpace: "nowrap" }}>{`**** **** **** ${card.last4}`}</Text></td>
+                      <td align={'center'}>
                         {card.isLoading ? (
                           <Loader icon width="20px" height="20px" />
                         ) : (
-                          <IconButton onClick={_handleRemoveCard(card)}>
-                            <Icon name="bin" style={{ fill: '#51C482' }} />
-                          </IconButton>
-                        )}
+                            <IconButton onClick={_handleRemoveCard(card)}>
+                              <Icon name="bin" style={{ fill: '#51C482' }} />
+                            </IconButton>
+                          )}
                       </td>
                     </tr>
                   ))}
@@ -210,8 +210,8 @@ const CheckoutPage = ({ match, location, history, ...props }) => {
               </Table>
             </>
           ) : (
-            <Text>You don't have any credit cards yet, please add one :)</Text>
-          )}
+              <Text>You don't have any credit cards yet, please add one :)</Text>
+            )}
 
           <Button size="sm" onClick={_addNewCard} isLoading={isCreating}>
             Add Card
