62685ce710afba08406b91de002e8da4f57ae1a1
Arthemus <arthemus.moreira@gmail.com>
2019-09-18 15:43:00 +1000

    Reset and verify email.

--- src/pages/Account/ProfilePage/index.js
+++ src/pages/Account/ProfilePage/index.js
@@ -2,27 +2,35 @@ import React, { useEffect, Fragment } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import Helmet from 'react-helmet'
 import { Loader } from 'components'
-import FormProfile from './FormProfile'
 
 import { onGetProfile } from 'redux/ducks/account'
 
-const ProfilePage = ({ ...props }) => {
+import FormProfile from './FormProfile'
 
+const ProfilePage = ({ ...props }) => {
   const dispatch = useDispatch()
 
-  const { user: { id } } = useSelector(state => state.account.get)
-  const { isLoading, get: { user } } = useSelector(state => state.account)
+  const queryParams = new URLSearchParams(props.location.search)
+  const confirmationToken = queryParams.get('confirmation')
+
+  const {
+    user: { id }
+  } = useSelector(state => state.account.get)
+
+  const {
+    isLoading,
+    get: { user }
+  } = useSelector(state => state.account)
 
   useEffect(() => {
-    dispatch(onGetProfile(id))
-  }, [dispatch, id])
+    dispatch(onGetProfile(id, confirmationToken))
+  }, [dispatch, id, confirmationToken])
 
   return (
     <Fragment>
       <Helmet title="Account Profile - Spacenow" />
       {isLoading && <Loader text="Loading Profile..." />}
-      {user && <FormProfile user={user} dispatch={dispatch}></FormProfile>}
-
+      {user && <FormProfile user={user} dispatch={dispatch} />}
     </Fragment>
   )
 }
