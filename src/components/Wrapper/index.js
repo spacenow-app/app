36f70d8d37727133478e728ea2bb81beaf9a2db7
Camila <kmi_218@hotmail.com>
2019-09-18 14:32:25 +1000

    add padding to site Wrapper component

--- src/components/Wrapper/index.js
+++ src/components/Wrapper/index.js
@@ -4,6 +4,8 @@ import styled from 'styled-components'
 import { space, layout } from 'styled-system'
 
 const ContainerStyled = styled(Container)`
+  padding-right: 20px !important;
+  padding-left: 20px !important;
   &&& {
     ${space}
     ${layout}
