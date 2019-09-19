9d8914f272ab82a2595467fab2d88668a696892e
Bruno <brunodasilvalenga@gmail.com>
2019-09-19 15:13:17 +1000

    include search in navbat

--- src/routes/Space/index.js
+++ src/routes/Space/index.js
@@ -1,17 +1,16 @@
 import React, { Suspense, lazy } from 'react'
 import PropTypes from 'prop-types'
 import { Switch, Route } from 'react-router-dom'
-import {NavBar} from 'components'
+import { NavBar } from 'components'
 
 const SpacePage = lazy(() => import('pages/Space/SpacePage'))
 const PartnerPage = lazy(() => import('pages/Space/PartnerPage'))
 const NotFoundPage = lazy(() => import('pages/NotFoundPage'))
 
 const Space = ({ match, ...props }) => {
-
   return (
     <Suspense fallback={<div>Loading...</div>}>
-    <NavBar />
+      <NavBar shownSearch {...props} />
       <Switch>
         <Route exact component={SpacePage} path={`${match.path}/:id`} />
         <Route exact component={PartnerPage} path={`${match.path}/partner/:id`} />
