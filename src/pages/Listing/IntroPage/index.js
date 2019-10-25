ab371924ebca6d2ee09b3039b49bb1ef342c1d72
Camila <kmi_218@hotmail.com>
2019-10-25 14:34:33 +1100

    change intro page

--- src/pages/Listing/IntroPage/index.js
+++ src/pages/Listing/IntroPage/index.js
@@ -1,15 +1,16 @@
 import React, { Component } from 'react'
 import styled from 'styled-components'
 import Helmet from 'react-helmet'
-import { Wrapper, Card, Title, Footer } from 'components'
+import { Wrapper, Card, Title, Footer, Text } from 'components'
 import { config } from 'variables'
 // import FormContactUs from './FormContactUs'
 
 const WrapperStyled = styled.div`
   display: grid;
-  grid-template-columns: auto auto;
+  grid-template-columns: 1fr 1fr;
   grid-gap: 20px;
   margin-bottom: 50px;
+  margin-top: 50px;
   @media (max-width: 680px) {
     grid-template-columns: auto;
     margin-bottom: 20px;
@@ -29,20 +30,20 @@ class IntroPage extends Component {
     return (
       <Wrapper>
         <Helmet title="Listing Intro - Spacenow" />
-        <Title
-          type="h3"
-          title="Are you creating a single space or managing multiple?"
-          subtitle="We’ve created two listing options for quicker space management. If you are only listing one space please select single space. If you have more than one, ie a meeting room, car park and office space please select the multiple spaces."
-        />
+        <Title type="h3" title="Want to list one at a time or multiple?" />
+        <Text>
+          We’ve created two listing options for quicker space management. <br />
+          Multiple spaces is for spaces that share the same address, ie. an office and a meeting room.
+        </Text>
         <WrapperStyled>
           <Card
             border
             rounded
             type="icon"
-            icon="single-space"
-            title="Single Space"
-            description="If you’re only listing one space this is for you. Ie. a single car space, meeting room, event space, or storage area.<separate>TIP: If you’re a coworking space please use the multiple spaces link to the right."
-            buttonText="Create"
+            icon="amenitie-storeRoom"
+            title="One at a time"
+            description="Simple option to list one at a time. With a preview page before publishing. <separate> TIP: Manage your own space"
+            buttonText="Get started"
             buttonHandleClick={() => this._goTo('single')}
           />
 
@@ -51,10 +52,11 @@ class IntroPage extends Component {
             rounded
             border
             type="icon"
-            icon="multiple-space"
+            icon="amenitie-storeRoom"
+            icon2="amenitie-storeRoom"
             title="Multiple spaces"
-            description="If you're listing more than one or 2 spaces, please contact us and we will be happy to help get everything set up on your behalf – get ready for the smoothest on boarding ever.<separate>"
-            buttonText="Contact Us"
+            description="Best option for listing multiple spaces with same address. <separate> TIP: Talk with one of our account managers"
+            buttonText="Contact us"
             buttonHandleClick={() => this._goTo('multiple')}
           />
         </WrapperStyled>
