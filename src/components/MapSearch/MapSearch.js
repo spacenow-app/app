2b4b52da9b0ef05d3207e1af8854c42ad813dc54
Arthemus <arthemus.moreira@gmail.com>
2019-11-05 12:50:55 +1100

    Clean code.

--- src/components/MapSearch/MapSearch.js
+++ src/components/MapSearch/MapSearch.js
@@ -1,85 +1,11 @@
 import React, { memo } from 'react'
-import styled from 'styled-components'
 import PropTypes from 'prop-types'
-import { withGoogleMap, GoogleMap, InfoWindow } from 'react-google-maps'
+import { withGoogleMap, GoogleMap } from 'react-google-maps'
 import MarkerWithLabel from 'react-google-maps/lib/components/addons/MarkerWithLabel'
-import { Avatar, Price } from 'components'
+
+import { Price } from 'components'
+
 import defaultMapStyle from './default_map_style.json'
-
-const CardContainer = styled.div`
-  display: grid;
-  grid-template-columns: auto 1fr;
-  grid-column-gap: 10px;
-  height: 100px;
-  margin-right: 20px;
-  min-width: 350px;
-
-  :before {
-    content: ' ';
-    width: 0;
-    height: 0;
-    border-left: 10px solid transparent;
-    border-right: 10px solid transparent;
-    border-bottom: 10px solid #fff;
-    position: absolute;
-    top: -10px;
-    left: 130px;
-  }
-`
-
-const CardImage = styled.div`
-  background-image: url(${props => props.src});
-  width: 100px;
-  height: 100px;
-  background-repeat: no-repeat;
-  background-size: cover;
-  margin-left: -1px;
-  cursor: pointer;
-`
-
-const CardContent = styled.div`
-  line-height: 1.8rem;
-  padding-bottom: 5px;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-`
-
-const CardContentTitle = styled.span`
-  display: block;
-  font-size: 16px;
-  font-family: 'Montserrat-SemiBold';
-  white-space: nowrap;
-  overflow: hidden;
-  text-overflow: ellipsis;
-  width: 245px;
-`
-
-const CardContentTextPrice = styled.span`
-  display: block;
-  font-size: 14px;
-  font-family: 'MontSerrat-Medium';
-`
-
-const CardContentPrice = styled.span`
-  font-family: 'MontSerrat-Bold';
-`
-
-const MarkerStyled = styled.div`
-background-color: green;
-font-size: 16px;
-padding: 8px 14px;
-borderRadius: 50px;
-color: #172439;
-border: 1px solid #CBCBCB;
-font-family: Montserrat-Bold;
-`
-
-const CardContentUserTitle = styled.span`
-  font-size: 12px;
-  font-family: 'MontSerrat-Medium';
-  margin-left: 5px;
-`
 
 const MapSearch = withGoogleMap(props => {
   return (
@@ -118,49 +44,27 @@ const MapSearch = withGoogleMap(props => {
                   size="16px"
                 />
               }
-              zIndex={(props.selectedMarker && props.selectedMarker.id === marker.id) ? 2 : 1}
+              zIndex={props.selectedMarker && props.selectedMarker.id === marker.id ? 2 : 1}
               labelStyle={{
-                backgroundColor: props.selectedMarker ? props.selectedMarker.id === marker.id ? '#6adc91' : '#ffffff' : '#ffffff',
+                backgroundColor: props.selectedMarker
+                  ? props.selectedMarker.id === marker.id
+                    ? '#6adc91'
+                    : '#ffffff'
+                  : '#ffffff',
                 fontSize: '16px',
                 padding: '8px 14px',
                 borderRadius: '50px',
-                color: props.selectedMarker ? props.selectedMarker.id === marker.id ? '#ffffff' : '#172439' : '#172439',
+                color: props.selectedMarker
+                  ? props.selectedMarker.id === marker.id
+                    ? '#ffffff'
+                    : '#172439'
+                  : '#172439',
                 border: '1px solid #CBCBCB',
                 fontFamily: 'Montserrat-Bold'
               }}
             />
           </>
         ))}
-
-      { // props.selectedMarker && (
-        // <InfoWindow
-        //   onCloseClick={() => {
-        //     props.onClickMarker(null)
-        //   }}
-        //   position={{
-        //     lat: props.selectedMarker.lat,
-        //     lng: props.selectedMarker.lng
-        //   }}
-        // >
-        //   <CardContainer>
-        //     <CardImage
-        //       src={props.selectedMarker.photo}
-        //       onClick={() => props.history.push(`/space/${props.selectedMarker.id}`)}
-        //     />
-        //     <CardContent>
-        //       <CardContentTitle>{props.selectedMarker.title}</CardContentTitle>
-        //       <CardContentTextPrice>
-        //         From <CardContentPrice>{props.selectedMarker.price}</CardContentPrice> {props.selectedMarker.period}
-        //       </CardContentTextPrice>
-        //       <CardContentUser>
-        //         <Avatar width="30px" height="30px" image={props.selectedMarker.host.photo} />
-        //         <CardContentUserTitle>{props.selectedMarker.host.name}</CardContentUserTitle>
-        //       </CardContentUser>
-        //     </CardContent>
-        //   </CardContainer>
-        // </InfoWindow>
-        //)
-      }
     </GoogleMap>
   )
 })
@@ -195,11 +99,9 @@ const comparisonFn = (prevProps, nextProps) => {
   if (!(prevProps.markers === nextProps.markers)) {
     return false
   }
-
   if (!(prevProps.position === nextProps.position)) {
     return false
   }
-
   return prevProps.selectedMarker === nextProps.selectedMarker
 }
 
