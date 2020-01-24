589e5eb8b1c352e110f8b41d31990ffed7653221
Camila <kmi_218@hotmail.com>
2020-01-24 15:45:17 +1100

    timed out

--- src/pages/Checkout/InfoTab/index.js
+++ src/pages/Checkout/InfoTab/index.js
@@ -152,7 +152,7 @@ const InfoTab = ({ match, location, history, ...props }) => {
           <TimeTable data={listing.accessDays.listingAccessHours} />
 
           <Box mt="100px" mb="25px" display="flex">
-            <ButtonStyled onClick={() => _goToNext()}>Agree and continue</ButtonStyled>
+            <ButtonStyled onClick={() => history.replace(`pay`)}>Agree and continue</ButtonStyled>
           </Box>
         </Cell>
         <Cell area="card">
