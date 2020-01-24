8f1e5820f2748c7c331c32a9fc9097a868b8394d
Arthemus <arthemus.moreira@gmail.com>
2020-01-24 11:22:06 +1100

    Local test for Facebook login.

--- src/components/ButtonSocial/ButtonFacebook.js
+++ src/components/ButtonSocial/ButtonFacebook.js
@@ -24,6 +24,7 @@ const ButtonStyled = styled.button`
 `
 
 const ButtonSocial = ({ onResponse, isDisabled, ...props }) => {
+  console.log('Facebook ID: ', config.facebook_app_id)
   return (
     <FacebookLogin
       appId={config.facebook_app_id}
